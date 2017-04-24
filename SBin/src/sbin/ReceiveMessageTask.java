db9b489aeb0edb10e7c9dfed23b6842b0a42c271
karbulot <adam2070@gmail.com>
2017-04-25 00:22:28 +0200

    ReceiveMessageTask w SBinServer z jednym argumentem tak jak miało być

--- SBin/src/sbin/ReceiveMessageTask.java
+++ SBin/src/sbin/ReceiveMessageTask.java
@@ -12,7 +12,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.Socket;
-import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.concurrent.Task;
@@ -23,11 +22,10 @@ import javafx.concurrent.Task;
  */
 public class ReceiveMessageTask extends Task<Void> {
     Socket socket;
-    File file;
  
-    public ReceiveMessageTask(Socket socket, File file) {
+    public ReceiveMessageTask(Socket socket) {
+        System.out.println("ReceiveMessageTask create");
         this.socket = socket;
-        this.file = file;
     }
  
     @Override 
@@ -35,18 +33,14 @@ public class ReceiveMessageTask extends Task<Void> {
         System.out.println("ReceiveMessageTask start");
         try (DataInputStream input = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
             ByteArrayOutputStream output = new ByteArrayOutputStream()) {
-            byte[] buffer = new byte[4096]; //bufor 4KB
+            byte[] buffer = new byte[4]; //bufor 4KB
             int readSize;
             while ((readSize = input.read(buffer)) != -1) {
-
-                    for (int i = 0; i < readSize; i++){
-                        System.out.print(Arrays.toString(buffer));
-                    }
                     output.write(buffer, 0, readSize);
                 }
             System.out.println("start translate msg");
             TranslateMessageTask translate = 
-                    new TranslateMessageTask(output.toByteArray(), new File("D:\\received\\log.txt"));
+                    new TranslateMessageTask(output.toByteArray(), new File("./log.txt"));
             try {
                 translate.Translate();
             } catch (FileNotFoundException e){
