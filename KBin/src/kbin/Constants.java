40ed6c4884173c45209367cfb43ac76ab96a79ba
karbulot <adam2070@gmail.com>
2017-06-03 01:10:15 +0200

    new file:   KBin/src/kbin/Constants.java

---
+++ KBin/src/kbin/Constants.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kbin;
+
+/**
+ *
+ * @author adam
+ */
+public class Constants {
+   private static Constants instance = null;
+   protected Constants(){}
+   public Constants getInstance() {
+       if (instance == null){
+           instance = new Constants();
+       }
+       return instance;
+   }
+   public static final int TIME = 60;
+}
